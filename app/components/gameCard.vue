5213625e6177010552689ecf4be45b8ebea2bd90
George Finch <georgefinch12@gmail.com>
2025-08-14 14:37:23 +0100

    connected to back end.
    system now contact server, and use said api to populate the UI.

--- app/components/gameCard.vue
+++ app/components/gameCard.vue
@@ -1,17 +1,23 @@
+<script setup>
+
+defineProps(['name','player','description','imgAlt','img'])
+
+
+</script>
 <template>
     <div class="p-[20px] bg-secondary w-[200px] h-[300px] rounded-lg flex flex-col ">
-        <img src="/assets/place holder img.jpg"/>
+       <img :src=img :alt=imgAlt /> 
         <div class="flex flex-col justify-between">
             <div class="flex flex-row justify-between">
-            <h2>name</h2>
+            <h2>{{ name }}</h2>
              <a>
                 options
                 </a>
             </div>
-            <p>plater</p>
+            <p>{{ player }}</p>
             
                 <p>
-                description
+                {{description}}
                 </p>
                
             
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,27 @@
+<script setup >
+let name = ref("loading");
+let player = ref("loading");
+let description = ref("loading");
+
+let imgRef = ref("")
+let imgAlt=ref("boardGameImg")
+
+ fetch('http://localhost:8080/games')
+            .then(res=>res.json())            
+            .then(json=>{ 
+                name.value=json.name;
+                 player.value=json.playerCount;
+                  description.value=json.description;
+            imgRef.value=json.base64
+            imgAlt.value=json.imgAlt
+            console.log(imgRef.value)
+            console.log(json)
+            } )
+            
+</script>
+
 <template>
 <div class="bg-primary h-screen w-full">
-<GameCard></GameCard>
+<GameCard :name=name :player=player :description=description :img=imgRef :imgAlt=imgAlt ></GameCard>
 </div>
 </template>
