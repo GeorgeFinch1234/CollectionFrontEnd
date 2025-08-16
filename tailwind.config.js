140b6b1da06cc467275f0f19010e0e37d238a1b4
George Finch <georgefinch12@gmail.com>
2025-08-16 23:29:07 +0100

    updated color scheme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        /*
         primary: '#265277',      // you can now use bg-primary, text-primary
         secondary: '#D9D9D9',
         alt :'#AEFF8B'
+       primary: '#0F0E47',      // you can now use bg-primary, text-primary
+        secondary: '#8686AC',
+        alt :'#272757'
+          primary: '#A8DCAB',      // you can now use bg-primary, text-primary
+        secondary: '#519755',
+        alt :'#DBAAA7'
+        primary: '#331C08',      // you can now use bg-primary, text-primary
+        secondary: '#664236',
+        alt :'#CCBEB1'
+        
+        primary: '#496580',      // you can now use bg-primary, text-primary
+        secondary: '#BADDFF',
+        alt :'#FFDBBB'
+        
+        */
+       primary: '#496580',      // you can now use bg-primary, text-primary
+        secondary: '#BADDFF',
+        alt :'#FFDBBB'
+        
       },
     }
 }
