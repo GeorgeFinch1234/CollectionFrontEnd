6a15d05b44dea1b4a2ee7e9ea1cbde385c302e2b
George Finch <georgefinch12@gmail.com>
2025-08-28 10:13:00 +0100

    middleware.
    added in middleware, so that user now has to be logged in to be able to access the website content.

---
+++ app/middleware/accessTokenCheck.global.js
@@ -0,0 +1,26 @@
+import {useTokenStore} from'~/utils/test.js'
+
+
+
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  
+  const tokenStore = useTokenStore()
+// so if they have access its fine to continue so can skip all this stuff
+if(tokenStore.token == ""){
+    //even if not logged in going to login is fine or signup
+if (to.path == "/" || to.path=="/signup" ) {
+    
+       return 
+  }else{
+    //sends them to login page
+    return navigateTo('/')
+  }
+}
+
+  
+  
+  
+  
+    
+})
